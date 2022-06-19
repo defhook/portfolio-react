8d7b6c969c84f70ad396b909daf2d904e89b3391
defhook <defhook152@gmail.com>
2022-06-19 14:08:39 -0700

    updated about index with short bio

--- src/App.js
+++ src/App.js
@@ -1,23 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import Nav from "./components/Nav";
+import About from "./components/About";
+import Portfolio from "./component/Portfolio";
+import ContactForm from "./components/Contact";
 
 function App() {
+  const [categories] = useState([
+    {
+      name: "Full Stack Developer",
+      description:
+        
+    },
+  ]);
+
+  const [currentCategory, setCurrentCategory] = useState(categories[0]);
+  // This prevents the contact form from showing when a user initially navigates to the homepage.
+  const [contactSelected, setContactSelected] = useState(false);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Nav
+        categories={categories}
+        setCurrentCategory={setCurrentCategory}
+        currentCategory={currentCategory}
+        contactSelected={contactSelected}
+        setContactSelected={setContactSelected}
+      ></Nav>
+      <main>
+        {!contactSelected ? (
+          <>
+            <Portfolio currentCategory={currentCategory}></Portfolio>
+            <About></About>
+          </>
+        ) : (
+          <ContactForm></ContactForm>
+        )}
+      </main>
+      <footer></footer>
     </div>
   );
 }
---
+++ src/components/About/index.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import coverImage from "../../assets/cover/3dboxes.jpg";
+
+function About() {
+
+  return (
+    <section className="my-5" >
+      <h1 id="about">Who am I?</h1>
+      <img src={coverImage} className="my-2" style={{ width: "100%" }} alt="cover" />
+      <div className="my-2">
+        <p>
+        My name is Alvin. I have been working in the insurance industry for a little bit over a decade. I have recently started working on databases and creating reports based on analytics. I have always been a technology driven person and have recently started a Full-Stack Web Developer certificate program through the University of Arizona. I enjoy tackling problems and resolving them. I love the logic and structure of coding. I look forward to growing in the coding platform with my new found skill. 
+      </p>
+      </div>
+    </section>
+  )
+}
+
+export default About
